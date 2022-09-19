838c614a8de175f635f3e0f7b2b36c281a785730
Davit Mkhitaryan <dmkhitar@c2r6s5.42yerevan.am>
2022-09-19 22:07:27 +0400

    server text showing

---
+++ client.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <unistd.h>
+#include <signal.h>
+#include <stdio.h>
+
+int main(int ac, char ** argv)
+{
+    int pid = atoi(argv[1]);
+    char *str;
+
+    str = argv[2];
+    int i = 0;
+    int j;
+
+    while (str[i] != '\0')
+    {
+        j = 7;
+        while (j >= 0)
+        {
+            if ((str[i] >> j & 1))
+                kill(pid, SIGUSR1);
+            else
+                kill(pid, SIGUSR2);
+            usleep(100);
+            j--;
+        }
+        i++;
+    }
+    return (0);
+}
---
+++ server.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <signal.h>
+
+static void handler(int sig, siginfo_t *info, void *context)
+{
+    static char c;
+    static int i = 0;
+
+	(void)context;
+	if (sig == SIGUSR1)
+        c = c | 1;
+    if (++i == 8)
+    {
+		write(1, &c, 1);
+        c = 0;
+        i = 0;
+    }
+	else
+	{
+		c <<= 1;
+	}
+}
+
+int main()
+{
+    struct sigaction sig_flags;
+
+    sig_flags.sa_sigaction = handler;
+	sig_flags.sa_flags = SA_SIGINFO;
+    sig_flags.sa_flags = SA_RESTART;
+
+    printf("%d\n", getpid());
+    sigaction(SIGUSR1, &sig_flags, 0);
+    sigaction(SIGUSR2, &sig_flags, 0);
+    while (1);
+}
